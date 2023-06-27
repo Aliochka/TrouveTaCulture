bbc2255dea41f31236bf4969712fd4a772e0aa86
Romain Chaffal <romain.chaffal@passculture.app>
2023-06-27 11:19:14 +0200

    feat: add geoloc

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
+import { useState, useEffect } from 'react'
 import styles from './App.module.scss'
 import Map from './components/Map.tsx'
 
 function App() {
+  const [latitude, setLatitude] = useState<number | undefined>(undefined)
+  const [longitude, setLongitude] = useState<number | undefined>(undefined)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+        console.log('coordonnées', latitude, longitude)
+      },
+      error => {
+        // Géolocalisation non autorisée ou erreur
+        console.error(error)
+      }
+    ),
+      []
+  })
+
   return (
     <div className={styles['content']}>
       <h1>Trouve ta culture</h1>
-      <div className={styles['card']}>
-        <button>Me géololocaliser</button>
-      </div>
       <p className={styles['explanations']}>
         Utilise la carte pour trouver des lieux culturels autour de toi
       </p>
-      <Map />
+      <Map latitude={latitude} longitude={longitude} />
     </div>
   )
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,11 +2,16 @@ import { useEffect, useRef } from 'react'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
-function Map() {
+interface MapProps {
+  latitude?: number
+  longitude?: number
+}
+
+function Map({ latitude = 48.505, longitude = 2.49 }: MapProps) {
   const mapRef = useRef<HTMLDivElement | string>('')
 
   useEffect(() => {
-    const map = L.map(mapRef.current).setView([48.505, 2.49], 8)
+    const map = L.map(mapRef.current).setView([latitude, longitude], 15)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
@@ -16,7 +21,7 @@ function Map() {
     return () => {
       map.remove()
     }
-  }, [])
+  }, [latitude, longitude])
 
   // @ts-expect-error - mapRef type is incorrerect
   return <div ref={mapRef} style={{ width: '100%', height: '800px' }}></div>
