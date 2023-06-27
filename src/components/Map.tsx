82998971f6c0aad5041758becff910f5ded7be98
Romain Chaffal <romain.chaffal@passculture.app>
2023-06-27 20:01:21 +0200

    style: of Map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -14,10 +14,27 @@ function Map({ latitude = 48.505, longitude = 2.49 }: MapProps) {
   useEffect(() => {
     const map = L.map(mapRef.current).setView([latitude, longitude], 15)
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    }).addTo(map)
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   attribution:
+    //     'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    // }).addTo(map)
+
+    L.tileLayer(
+      'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, Tiles &copy; <a href="https://carto.com/">CARTO</a>',
+        subdomains: 'abcd',
+      }
+    ).addTo(map)
+
+    // L.tileLayer('https://a.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    //   attribution:
+    //     'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, Tiles by <a href="https://openstreetmap.se/">OSM Bright</a>',
+    //   subdomains: 'abc',
+    // }).addTo(map)
+
+    L.marker([49.505, 2.49]).addTo(map).bindPopup('KIKOU')
 
     return () => {
       map.remove()
