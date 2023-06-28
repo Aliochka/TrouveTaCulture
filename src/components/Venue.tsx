a3672d7bd5f301235781c6cc193de3fe04038fa4
Chouteau <tetyana.chouteau@passculture.app>
2023-06-28 11:02:44 +0200

    Création de la page Lieu, ajout d'une image de Lieu

--- src/components/Venue.tsx
+++ src/components/Venue.tsx
@@ -1,9 +1,26 @@
+import React from 'react'
+
 function Venue() {
   return (
     <div className="lieu">
       <h1>Page de lieu </h1>
       <h2>ZooParc</h2>
-      <p>Description</p>
+      <p>
+        À travers son association Beauval Nature, Beauval soutient des actions
+        dans le monde entier afin de préserver le milieu naturel, protéger les
+        espèces animales et végétales, favoriser les études scientifiques et le
+        partage de connaissances. Nouveau programme de conservation confié à
+        Beauval Nature, Help Congo vient en aide aux chimpanzés dans le parc
+        national de Conkouati-Douli depuis plus de 30 ans. Missions de
+        protection des primates, travaux de recherche, recensement de la faune
+        locale, sensibilisation des populations… les projets à mener sont très
+        nombreux par les équipes de Beauval Nature dépêchées sur place et les
+        équipes locales. Une reconnaissance supplémentaire du professionnalisme
+        de Beauval Nature dans la conservation d’espèces !
+      </p>
+
+      <img src="/src/assets/zoo.png" alt="ZooParc" />
+
       <img src="/src/assets/lieu.png" alt="Lieu" />
     </div>
   )
